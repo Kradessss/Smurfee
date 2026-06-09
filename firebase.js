e66d726e7e20af46f295b02cd79373deca42c407
Kradessss <diarotadankerubin@gmail.com>
2026-06-09 21:12:15 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,31 +1,85 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import {
-    getFirestore,
-    collection,
-    addDoc,
-    getDocs,
-    query,
-    where
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+getFirestore,
+collection,
+addDoc,
+getDocs,
+query,
+where,
+doc,
+setDoc,
+getDoc
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import {
+getAuth,
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword,
+signOut,
+onAuthStateChanged
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDWQaqR6JsM1PDm13vMDuJLnZcIn7GPpZE",
-    authDomain: "smurfee-5afe7.firebaseapp.com",
-    projectId: "smurfee-5afe7",
-    storageBucket: "smurfee-5afe7.firebasestorage.app",
-    messagingSenderId: "658384225361",
-    appId: "1:658384225361:web:acdf95c219f2779e86a470"
+
+
+apiKey: "AIzaSyDWQaqR6JsM1PDm13vMDuJLnZcIn7GPpZE",
+
+authDomain:
+"smurfee-5afe7.firebaseapp.com",
+
+projectId:
+"smurfee-5afe7",
+
+storageBucket:
+"smurfee-5afe7.firebasestorage.app",
+
+messagingSenderId:
+"658384225361",
+
+appId:
+"1:658384225361:web:acdf95c219f2779e86a470"
+
+
 };
 
-const app = initializeApp(firebaseConfig);
+const app =
+initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+/* FIRESTORE */
+
+export const db =
+getFirestore(app);
+
+/* AUTH */
+
+export const auth =
+getAuth(app);
+
+/* EXPORTS */
 
 export {
-    collection,
-    addDoc,
-    getDocs,
-    query,
-    where
+
+
+collection,
+addDoc,
+getDocs,
+query,
+where,
+
+doc,
+setDoc,
+getDoc,
+
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword,
+
+signOut,
+
+onAuthStateChanged
+
+
 };
